db4fa70f774a26dc312a949ca053f59f93fa8042
jcristy <jcristy@vt.edu>
2022-04-11 21:43:26 -0400

    Identifies a boring name and modifies it

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ const main = (): void => {
 
 };
 
+const boringNameRegex : RegExp = /.*1/;
+
 // Day's check for stalls to name
 var daySubscription =
         context.subscribe('interval.day', function() {
@@ -21,9 +23,14 @@ var rideRatingSubscription =
 			console.log(`Ride ${iride.name} type ${iride.classification} exc ${iride.excitement}`);
 			switch (iride.classification){
 				case "ride": 
-					let obj: RideObject = iride.object
-					if (IsRollerCoaster(obj.rideType[0])) { // not sure why it's an array nor what's in the other 2 elements that seemed to always be 255
+					let rideDefinition: RideObject = iride.object
+					if (IsRollerCoaster(rideDefinition.rideType[0])) { // not sure why it's an array nor what's in the other 2 elements that seemed to always be 255
 						console.log('a roller coaster');
+						if (boringNameRegex.test(iride.name)) {
+							console.log('had a boring name');
+							iride.name = 'a cool name';
+						}
+						
 					} else {
 						console.log('not a roller coaster');
 						// If a ride is not named <flat type> 1, <flat type> 1 will be reused :-/
