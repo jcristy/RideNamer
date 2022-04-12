999d7946ff3fbc606ea1d974ef1c206728f4f2b6
jcristy <jcristy@vt.edu>
2022-04-11 21:31:03 -0400

    Can identify roller coasters

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,15 @@
 /// <reference path="../lib/openrct2.d.ts" />
 
 export const isUiAvailable = typeof ui !== 'undefined';
+
+export function IsRollerCoaster(rideType: number) : boolean {
+	// Based off of https://github.com/OpenRCT2/OpenRCT2/blob/605112297d0681f0c85e2de185358f350104c282/src/openrct2/ride/Ride.h#L485
+	let rollerCoasterTypes: number[] = [0,1,2,3,4,7,9,10,13,15,17,19,42,44,51,52,53,54,55,56,57,58,59,62,64,65,66,68,73,74,75,76,86,87,91,92,94,95,96,97];
+	for (let rct of rollerCoasterTypes) {
+		if (rideType < rct+.01 && rideType > rct-.01) {
+			return true;
+		}
+	}
+
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,45 @@
 import {
+	IsRollerCoaster
 } from './helpers';
 
 const main = (): void => {
   console.log(`Your plug-in has started!`);
-  let rides: Ride[] = map.rides
-  for (let iride of rides) {
-    console.log(`Ride ${iride.name}`);
-  }
 
 };
+
+// Day's check for stalls to name
+var daySubscription =
+        context.subscribe('interval.day', function() {
+		console.log('A day has passed');
+        });
+
+var rideRatingSubscription =
+        context.subscribe('ride.ratings.calculate', function() {
+		console.log('A ride has been rated');
+  		let rides: Ride[] = map.rides
+		for (let iride of rides) {
+			console.log(`Ride ${iride.name} type ${iride.classification} exc ${iride.excitement}`);
+			switch (iride.classification){
+				case "ride": 
+					let obj: RideObject = iride.object
+					if (IsRollerCoaster(obj.rideType[0])) { // not sure why it's an array nor what's in the other 2 elements that seemed to always be 255
+						console.log('a roller coaster');
+					} else {
+						console.log('not a roller coaster');
+						// If a ride is not named <flat type> 1, <flat type> 1 will be reused :-/
+					}
+					
+					break;
+				case "stall": 
+					break;
+				case "facility": 
+			}
+			// if it's a stall, do nothing
+
+			// if it's a flat ride, just remove the number
+
+			// if it's a roller coaster generate a name
+		}
+        });
 
 export default main;
